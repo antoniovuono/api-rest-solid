c9ec8d3304eaeea964a3cb087f5e3651c9cd1faf
Antonio Vuono <antoniosvuono@icloud.com>
2024-03-25 21:02:32 -0300

    test: validate late check-in use-case unity tests created

--- src/use-cases/validate-check-in.test.ts
+++ src/use-cases/validate-check-in.test.ts
@@ -1,7 +1,8 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
-import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { ValidateCheckInUseCase } from './validate-check-in'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let validateCheckInUseCase: ValidateCheckInUseCase
@@ -10,11 +11,11 @@ describe('Validate Check In Use Case', () => {
   beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository()
     validateCheckInUseCase = new ValidateCheckInUseCase(checkInsRepository)
-    // vi.useFakeTimers()
+    vi.useFakeTimers()
   })
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers()
   })
 
   it('should be able to validate the check-in', async () => {
@@ -37,5 +38,25 @@ describe('Validate Check In Use Case', () => {
         checkInId: 'inexistent-check-in-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-id',
+      user_id: 'user-id',
+    })
+
+    // 1000 ms * 60 = 1 minute * 21 = 21 minutes
+    const twentyMinutesInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyMinutesInMs)
+
+    await expect(() =>
+      validateCheckInUseCase.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError)
   })
 })
