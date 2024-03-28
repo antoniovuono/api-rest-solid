ade339039612265cd3438e2a66a695423687af26
Antonio Vuono <antoniosvuono@icloud.com>
2024-03-28 16:11:54 -0300

    feature: profile controller created

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,10 +1,17 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  // garante que a rota não possa ser chamada se o token não existir
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  console.log(request.user.sub)
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
