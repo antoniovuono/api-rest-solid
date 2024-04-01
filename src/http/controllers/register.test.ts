b0521019b6daa7fa854d5d6a5870a514bb952c0a
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-01 20:33:31 -0300

    test: test e2e register route

--- src/http/controllers/register.test.ts
+++ src/http/controllers/register.test.ts
@@ -1,3 +1,25 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
 
-test('ok', () => {})
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    // é preciso executar para o nosso app estar pronto para receber requisições
+    await app.ready()
+  })
+
+  afterAll(() => {
+    // é preciso fechar a aplicação para liberar a porta
+    app.close()
+  })
+
+  it('should be able to register a new user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'jhondoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
