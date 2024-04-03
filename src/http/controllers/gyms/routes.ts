4c8619a1dbabfd5b4238aa4c5decf6ca80933b34
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-02 21:32:37 -0300

    feature: gyms routes created

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
+import { search } from './search'
+import { nearby } from './nearby'
+import { create } from './create'
 
 export async function gymsRoutes(app: FastifyInstance) {
   // todas as rotas que forem executadas pra baixo dessa linha serão rotas autenticadas
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('gyms', create)
 }
