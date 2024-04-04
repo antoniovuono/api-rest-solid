2f72c1b4cf4926708c704133d4080b20b7c640b4
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-03 21:32:54 -0300

    test: search controller e2e tested

---
+++ src/http/controllers/gyms/search.test.ts
@@ -0,0 +1,52 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Search Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search gym by title', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Gym Example',
+        description: 'Gym Description',
+        phone: '111111111',
+        latitude: -15.7972663,
+        longitude: -47.8967661,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Javascript Gym',
+        description: 'Description example',
+        phone: '111111111',
+        latitude: -15.7972663,
+        longitude: -47.8967661,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({ q: 'Javascript' })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({ title: 'Javascript Gym' }),
+    ])
+  })
+})
